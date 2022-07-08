bd074efb3a3a939533b80623b07b252c13838cd0
stopkyu <lyk9962@gmail.com>
2022-07-08 17:02:20 +0900

    add grouproom layout gruoproom actibity

---
+++ app/src/main/java/com/example/groupplanstudy/activities/GroupRoomActivity.java
@@ -0,0 +1,23 @@
+package com.example.groupplanstudy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.example.groupplanstudy.R;
+
+public class GroupRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.groupplanstudy.R.layout.activity_group_room);
+
+//        Toolbar grouproom_toolbar = getSupportActionBar(); //제목줄 객체 얻어오기
+//
+//        grouproom_toolbar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
