24490a274f17e4e29f1c5669e0c4c77ce61f5244
stopkyu <lyk9962@gmail.com>
2022-07-11 15:18:35 +0900

    add GroupQnaDto GroupRoomService GroupRoomAdapter grouproomqna_write

--- app/src/main/java/com/example/groupplanstudy/activities/GroupRoomActivity.java
+++ app/src/main/java/com/example/groupplanstudy/activities/GroupRoomActivity.java
@@ -7,12 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toolbar;
 
 import com.example.groupplanstudy.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GroupRoomActivity extends AppCompatActivity {
+    EditText groupqna_ettitle, groupqna_etcontent;
     private RecyclerView grouproom_recyclerView;
     LinearLayoutManager manager;
     private Long grId;
@@ -22,11 +24,16 @@ public class GroupRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FloatingActionButton grouproom_floatBtn = findViewById(R.id.grouproom_floatBtn);
         setContentView(R.layout.activity_group_room);
+
+        groupqna_ettitle = findViewById(R.id.groupqna_ettitle);
+        groupqna_etcontent = findViewById(R.id.groupqna_etcontent);
+
+        // 인텐트로 grId 추가
         Intent intent = getIntent();
         grId = intent.getLongExtra("grId", 0);
-
         Log.d("grId",grId+"");
 
+        //QnA 전체보기
 //        PhoneService phoneService
 //                = PhoneClient.getClient().create(PhoneService.class);
 //        Call<List<Phone>> call = phoneService.findAll();
@@ -51,6 +58,7 @@ public class GroupRoomActivity extends AppCompatActivity {
 //            }
 //        });
 
+        //QnA 글쓰기
 //        floatBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
